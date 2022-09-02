32985a4f7ec48540eba5041dd2d3e222c2950a25
Катя Рыжова <kataryzova@Air-Kata.lan>
2022-09-02 23:46:18 +0200

    Corrected mistake in 1st task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -7,8 +7,10 @@ int number2 = int.Parse(Console.ReadLine());
 if (number1 > number2) 
 { 
 Console.WriteLine($"max = {number1}"); 
+Console.WriteLine($"min = {number2}"); 
 } 
 else 
 { 
 Console.WriteLine($"max = {number2}"); 
+Console.WriteLine($"min = {number1}"); 
 }
