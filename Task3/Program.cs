dea3d59254ddeaf72b0263293fe20b27b984c5c6
Катя Рыжова <kataryzova@Air-Kata.lan>
2022-09-03 21:48:00 +0200

    Added 3rd task

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear(); 
+Console.Write("Write number "); 
+int number = int.Parse(Console.ReadLine()); 
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine("yes");
+}
+
+else
+{
+    Console.WriteLine("no");
+}
