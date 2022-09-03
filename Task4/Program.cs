b06e66cef0cafeefbffa16deb4ef7c7ec4dbad4c
Катя Рыжова <kataryzova@Air-Kata.lan>
2022-09-03 23:09:30 +0200

    Added 4th task

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear(); 
+Console.Write("Write number "); 
+int number = int.Parse(Console.ReadLine()); 
+
+int i;
+
+if (i = 1; i < number; ++i)
+{
+    if(i$2==0)
+       {
+        Console.WriteLine("i");
+       }
+}
